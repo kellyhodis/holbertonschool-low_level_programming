2d7e408fda6bee7f4c093424cf25c2a485d3d6ad
Kelly Hodis <668@holbertonschool.com>
2019-02-07 02:43:17 +0000

    Added Return value back in and updated function description to conform to Betty standards

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
 * main - Prints ""Programming is like building a multilingual puzzle"
+*
+* Return: 0
 */
-void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
