33b0287a851dda3cacc18ca6e513a962f8621b59
Kelly Hodis <668@holbertonschool.com>
2019-02-15 22:16:51 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_line - draws a straight line in the terminal
+* @n: tells function how many lines to draw
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
