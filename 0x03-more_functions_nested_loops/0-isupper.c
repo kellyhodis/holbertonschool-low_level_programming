0f55b7b050ef8e449ee8cefa9b8061db63c375a1
Kelly Hodis <668@holbertonschool.com>
2019-02-15 20:17:55 +0000

    Edited to conform to Betty syntax

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
 * _isupper  - returns 1 if the input is uppercase
+* @c: input to be checked
 *
 * Return: 0
 */
 int _isupper(int c)
 {
-	if (c > 64 && c <91)
+	if (c > 64 && c < 91)
 		return (1);
 	return (0);
 }
