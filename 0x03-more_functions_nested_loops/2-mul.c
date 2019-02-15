9676adea370846debc30f064797182b0cfddf823
Kelly Hodis <668@holbertonschool.com>
2019-02-15 20:26:05 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* mul -  function that multiplies two integers
+* @a: input to be multiplied
+* @b: input to be multiplied
+*
+* Return: 0
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
