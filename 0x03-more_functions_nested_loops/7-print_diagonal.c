74a50cc14b7f2b5bcfa05da9583502e8632fa129
Kelly Hodis <668@holbertonschool.com>
2019-02-16 01:06:34 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* print_diagonal - draws a diagonal line on the terminal
+* @n: input that tells function how long to make the line
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n == 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
