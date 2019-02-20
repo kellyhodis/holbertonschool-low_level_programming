09a3289d80f6070e2216d5b0c3c05d0dcb3eb1b3
Kelly Hodis <668@holbertonschool.com>
2019-02-20 20:49:43 +0000

    First commit

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _strlen - returns the length of a string
+* @s: input string
+*
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
