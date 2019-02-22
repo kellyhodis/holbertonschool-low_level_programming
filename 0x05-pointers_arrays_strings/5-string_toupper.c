876774f38b2b94b8357bfaef15fe4e571537d92b
Kelly Hodis <668@holbertonschool.com>
2019-02-22 04:25:04 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @s: string to manipulate
+*
+* Return: edited string
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+			s[i] -= 32;
+	}
+	return (s);
+}
