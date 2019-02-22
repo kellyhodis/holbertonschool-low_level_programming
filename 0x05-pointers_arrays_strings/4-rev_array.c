d5a4ca71146427ae7c263fecf5304230b7c8483a
Kelly Hodis <668@holbertonschool.com>
2019-02-22 04:01:24 +0000

    Edited to conform to Betty standards

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
 * reverse_array - reverses the content of an array of integers
 * @a: an array of integers
@@ -10,7 +9,7 @@
 void reverse_array(int *a, int n)
 {
 	int i, j, swap;
-	
+
 	for (i = 0, j = n - 1; i < n / 2; i++, j--)
 	{
 		swap = a[i];
