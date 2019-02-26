5dc895e7ad5ec5f25935e3a8c15cf42561b8d15f
Kelly Hodis <668@holbertonschool.com>
2019-02-26 05:44:56 +0000

    Removed an extraneous pointer

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,9 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
-
-	char *pdest = dest;
 	unsigned int i;
 
-
 	for (i = 0; i < n; i++)
-		src[i] = pdest[i];
-	return (pdest);
+		src[i] = dest[i];
+	return (dest);
 }
