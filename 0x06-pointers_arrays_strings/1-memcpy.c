fe656b65ac1ccf62ead08bb5345178685019f725
Kelly Hodis <668@holbertonschool.com>
2019-02-26 04:42:29 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _memcpy - copies the first n bytes of the memory area
+* pointed to by src to memory area pointed to by dest
+* @dest: memory area being copied to
+* @src: memory area being copied
+* @n: number of bytes in memory area being copied
+*
+* Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+
+	char *pdest = dest; 
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
+		src[i] = pdest[i];
+	return (pdest);
+}
