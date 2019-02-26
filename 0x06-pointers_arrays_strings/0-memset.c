7d39a389a5ea9899ab2ed7665e2bd54115905fed
Kelly Hodis <668@holbertonschool.com>
2019-02-26 03:57:45 +0000

    Edited to conform to Betty standards

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
 * @b: constant
 * @n: number of bytes in memory area pointed to by s
 *
-* Return: s 
+* Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -15,5 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 		s[i] = b;
 
-	return (s); 
-} 
+	return (s);
+}
