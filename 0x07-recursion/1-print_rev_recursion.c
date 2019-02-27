2bdaf0cd62c4c4ae16089992a62a50b5e42e521b
Kelly Hodis <668@holbertonschool.com>
2019-02-27 22:57:24 +0000

    Edited to conform to Betty standards

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* _print_rev_recursion - prints a string in reverse 
+* _print_rev_recursion - prints a string in reverse
 * @s: string to print
 *
-* Return: nothing 
+* Return: nothing
 */
 void _print_rev_recursion(char *s)
 {
