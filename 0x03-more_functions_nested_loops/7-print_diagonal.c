ef871490a6640c9ea2bd48014d45aa53194fd9c7
Kelly Hodis <668@holbertonschool.com>
2019-02-28 07:54:41 +0000

    Edited loop

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -7,11 +7,8 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n == 0)
+	if (n <= 0)
 		_putchar('\n');
-
-
-
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -22,4 +19,3 @@ void print_diagonal(int n)
 		_putchar('\n');
 	}
 }
-
