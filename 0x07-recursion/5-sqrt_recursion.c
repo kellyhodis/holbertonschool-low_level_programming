50f3f7cf663a9d3ed61aaf2525bb2f3c53d41f73
Kelly Hodis <668@holbertonschool.com>
2019-02-28 01:52:41 +0000

    Edited logic to account for negative square roots

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -10,9 +10,9 @@ int _sqrt(int n, int x)
 {
 	if (x * x == n)
 		return (x);
-	else if (x < 0)
+	else if (x > n / 2)
 		return (-1);
-	return (_sqrt(n, x - 1));
+	return (_sqrt(n, x + 1));
 }
 /**
 * _sqrt_recursion - function for finding the square root of variable
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 1)
 		return (-1);
-	return (_sqrt(n, n));
+	return (_sqrt(n, 0));
 }
