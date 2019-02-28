ff3269e9c2ade4e048e8067d42a97a6d0bec799d
Kelly Hodis <668@holbertonschool.com>
2019-02-28 01:43:33 +0000

    First commit

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* _sqrt - function for testing values against variable
+* @n: variable to test against
+* @x: testing values
+*
+* Return: test value that works
+*/
+int _sqrt(int n, int x)
+{
+        if (x * x == n)
+                return (x);
+        else if (x < 1)
+                return (-1);
+        return (_sqrt(n, x - 1));
+}
+/**
+* _sqrt_recursion - function for finding the square root of variable
+* @n: number to find square root of
+*
+* Return: square root
+*/
+int _sqrt_recursion(int n)
+{
+        if (n < 1)
+                return (-1);
+        return (_sqrt(n, n));
+}
