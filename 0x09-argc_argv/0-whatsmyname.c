7147e37ba5d6a6e34fea7b70414505574d72dfbf
Kelly Hodis <668@holbertonschool.com>
2019-03-02 04:09:35 +0000

    First commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints its name followed by a new line
+* @argc: argument count
+* @argv: argument vector; arg[0]
+*
+* Return: argc
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
