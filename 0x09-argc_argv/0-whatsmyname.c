bdb909262e2468b5c4578176872139e224ec3bc9
Kelly Hodis <668@holbertonschool.com>
2019-03-03 02:06:19 +0000

    Updated file

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include "holberton.h"
 /**
 * main - prints its name followed by a new line
 * @argc: argument count
 * @argv: argument vector; arg[0]
 *
-* Return: 0
+* Return: zero
 */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
-	printf("\n");	
 	return (0);
 }
