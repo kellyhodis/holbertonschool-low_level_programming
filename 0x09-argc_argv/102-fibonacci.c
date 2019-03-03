bcdf15f5dd535a6319efeefb3cbf34158756d504
Kelly Hodis <668@holbertonschool.com>
2019-03-03 03:01:48 +0000

    First commit

---
+++ 0x09-argc_argv/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+* followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	unsigned long n, m, hold;
+	int count;
+
+	n = 1, m = 2, count = 0;
+	while (count <= 50)
+	{
+		if (count == 50)
+			printf("%ld", n);
+		else
+			printf("%ld, ", n);
+		hold = n + m;
+		n = m;
+		m = hold;
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
