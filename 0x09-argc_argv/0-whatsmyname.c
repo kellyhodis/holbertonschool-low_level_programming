bdfbe8ff133dcd7a2139fee664f0e2c46c08d419
Kelly Hodis <668@holbertonschool.com>
2019-03-03 23:22:05 +0000

    Added base file

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the name of the program followed by a new line
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
