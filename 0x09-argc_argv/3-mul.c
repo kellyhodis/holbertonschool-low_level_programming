b9edb3b0355024dc9a6372ae1bb5b950a2a9dfd4
Kelly Hodis <668@holbertonschool.com>
2019-03-04 01:37:07 +0000

    Added control statement for negative numbers

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -8,26 +8,28 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2, i, j = 1, product;
+	int i, j = 1, product;
+	int num[2], flag[2];
 
-	num1 = 0, num2 = 0;
+	num[1] = 0, num[2] = 0;
 	while (j < argc)
 	{
 		for (i = 0; argv[j][i] != '\0'; i++)
 		{
 			if (argv[j][i] <= '9' && argv[j][i] >= '0')
 			{
-				if (j == 1)
-					num1 = num1 * 10 + argv[j][i] - '0';
-				if (j == 2)
-					num2 = num2 * 10 + argv[j][i] - '0';
+				num[j] = num[j] * 10 + argv[j][i] - '0';
 			}
+			else if (argv[j][i] == '-')
+				flag[j] = 1;	
 			else if (argv[j][i] > '9' || argv[j][i] < '0')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+		if (flag[j] == 1)
+			num[j] *= -1;
 		j++;
 	}
 	if (argc != 3)
@@ -35,7 +37,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	product = num1 * num2;
+	product = num[1] * num[2];
 	printf("%d\n", product);
 	return (product);
 }
