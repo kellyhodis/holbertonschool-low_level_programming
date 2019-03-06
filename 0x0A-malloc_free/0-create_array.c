64be1d03390da12b8befa8268b834360d7b9c8bd
Kelly Hodis <668@holbertonschool.com>
2019-03-06 05:20:54 +0000

    Edited syntax of malloc

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	array = malloc(size);
+	array = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
