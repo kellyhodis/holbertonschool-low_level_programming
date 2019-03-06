05d429f1ab7cc9930dab5c070a9daf7c9eaae791
Kelly Hodis <668@holbertonschool.com>
2019-03-06 06:02:58 +0000

    Edited order of statements

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -9,17 +9,18 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char *array = malloc(sizeof(char) * size);
 	unsigned int i;
 
 	if (size == 0)
 		return ('\0');
-	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 		if (!array || array[0] != c)
-		return ('\0');
+			return ('\0');
 	}
 	return (array);
 }
