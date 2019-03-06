179b2d9239e5e3b2a88fdcd09f8f98a112d1b44b
Kelly Hodis <668@holbertonschool.com>
2019-03-06 04:17:38 +0000

    First commit

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* create_array - creates array and initializes it with specific char
+* @size: size of the array
+* @c: specific char for initializing
+*
+* Return: pointer to array
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return ('\0');
+	array = malloc(size);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	if (!array || array[0] != c)
+		return ('\0');
+	return (array);
+}
