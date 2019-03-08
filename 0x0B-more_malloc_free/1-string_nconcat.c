0344212a6f21a736fa5c7726a8637f9397ff39fe
Kelly Hodis <668@holbertonschool.com>
2019-03-08 05:32:40 +0000

    Re-referenced pointer

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p = malloc(_strlen(s1) + n + 1);
 	else
 		p = malloc(_strlen(s1) + _strlen(s2) + 1);
-	if (!p[0])
+	if (!p)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
