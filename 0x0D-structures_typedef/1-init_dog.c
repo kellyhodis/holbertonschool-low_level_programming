505011cf8766aaf5405a086fdda348620bc2da9e
Kelly Hodis <668@holbertonschool.com>
2019-03-11 09:30:37 +0000

    First commit. Had to add DOG_H header file to main test program. Might have to edit again later to account for this

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "holberton.h"
+/**
+* init_dog - initializes a variable of type struct dog
+* @d: variable holding dog struct
+* @name: name of dog
+* @age: age of dog
+* @owner: name of dog's owner
+*
+* Return: nothing
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
