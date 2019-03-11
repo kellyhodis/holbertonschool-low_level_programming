f40557c2a0a06677d87dba4dc3bf2bfe4eeb7b1c
Kelly Hodis <668@holbertonschool.com>
2019-03-11 08:40:42 +0000

    Edited to conform to betty standards and changed return type

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 * main - prints the name of the current file
-* 
-* Return: nothing
+*
+* Return: 0
 */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
