6a3c8282a4d695e278d4f5bc29678941bda6e328
Kelly Hodis <668@holbertonschool.com>
2019-03-11 23:46:00 +0000

    Added variable declaration

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
 * init_dog - initializes a variable of type struct dog
 * @d: variable holding dog struct
@@ -10,6 +11,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
