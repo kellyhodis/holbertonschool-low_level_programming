cd2ffc871683e4c3f3ee562a83e319c269761fc2
Kelly Hodis <668@holbertonschool.com>
2019-03-11 10:38:54 +0000

    First commit

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* new_dog - creates a new dog
+* @name: name of dog
+* @age: age of dog
+* @owner: name of dog owner
+*
+* Return: dog if success or NULL
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+	char *dog_name = name;
+	char *dog_owner = owner;
+
+	doggo = malloc(sizeof(dog_t));
+	doggo->name = dog_name;
+	doggo->age = age;
+	doggo->owner = dog_owner;
+	if (!doggo)
+		return (NULL);
+	return (doggo);
+} 
