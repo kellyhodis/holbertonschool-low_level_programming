2720f729b0b19d5af63e4d6d5bd7bf7fe8304bc3
Kelly Hodis <668@holbertonschool.com>
2019-03-11 22:55:11 +0000

    Added documentation for struct and typedef declarations

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,12 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+* struct dog - holds information about a dog
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner of dog
+*
+* Description: creates instances of a dog
+*/ 
+struct dog 
+{
 	char *name;
 	float age;
 	char *owner;
 };
-
+/**
+* dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
