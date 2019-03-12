12dab15bb3be577fa7eb765a2f64c57a32ab4fef
Kelly Hodis <668@holbertonschool.com>
2019-03-12 00:48:35 +0000

    Removed extraneous variables

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,14 +10,12 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
-	char *dog_name = name;
-	char *dog_owner = owner;
+	dog_t *doggo = malloc(sizeof(dog_t));
 
 	doggo = malloc(sizeof(dog_t));
-	doggo->name = dog_name;
+	doggo->name = name;
 	doggo->age = age;
-	doggo->owner = dog_owner;
+	doggo->owner = owner;
 	if (!doggo)
 		return (NULL);
 	return (doggo);
