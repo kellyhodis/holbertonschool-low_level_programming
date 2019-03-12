48b30d361ad7e473558f6f4fe64125ba64488ee4
Kelly Hodis <668@holbertonschool.com>
2019-03-12 06:22:20 +0000

    Added condition for negative numbers

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n < 0)
+		return;
 	for (i = 0; i < n; i++)
 	{
-	if (i == n - 1)
-		printf("%d\n", a[i]);
-	else
-		printf("%d, ", a[i]);
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
 }
