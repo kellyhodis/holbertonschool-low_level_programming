632cd26931c2515340a00318fdc00b87a8d7976b
Kelly Hodis <668@holbertonschool.com>
2019-03-12 02:55:34 +0000

    Added condition to free memory if malloc fails

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);
 	if (!grid)
-		return (NULL);
+		return (free(grid), NULL);
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
