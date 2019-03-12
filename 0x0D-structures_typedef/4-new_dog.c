c7b8b99417b4af9c5bf95895f107b996976caf4d
Kelly Hodis <668@holbertonschool.com>
2019-03-12 00:58:21 +0000

    Changed struct initialization

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+char *_strcpy(char *dest, char *src);
 /**
 * new_dog - creates a new dog
 * @name: name of dog
@@ -10,13 +11,31 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo = malloc(sizeof(dog_t));
+	struct dog *doggo = malloc(sizeof(struct dog));
 
 	doggo = malloc(sizeof(dog_t));
+	if (!doggo)
+		return (NULL);
 	doggo->name = name;
 	doggo->age = age;
 	doggo->owner = owner;
-	if (!doggo)
-		return (NULL);
 	return (doggo);
+}
+/**
+* _strcpy - copies string pointed to by src to the buffer
+* @dest: destination
+* @src: source
+*
+* Return: pointer to array
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
