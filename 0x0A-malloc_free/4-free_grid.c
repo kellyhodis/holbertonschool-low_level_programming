662ba00a95dfe25d5e891361c243d0dc71b34010
Kelly Hodis <668@holbertonschool.com>
2019-03-12 03:25:37 +0000

    First commit

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* free_grid - frees a 2 dimensional grid previously created by alloc_grid
+* @grid: previously created grid
+* @height: number of rows in grid
+*
+* Return: Nothing
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = height - 1; i >= 0; i --)
+		free (grid[i]);
+	free (grid);
+}
