2bad3ed30b58063a568c4b3fc158af835ed629f6
kellyhodis <668@holbertonschool.com>
2019-03-13 19:46:53 -0400

    Update 3-main.c

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -11,30 +11,16 @@ int main(int argc, char *argv[])
 {
 	char ops[6] = {'+', '-', '*', '/', '%'};
 	int int1, int2, result, *;
-	char *o;
+	char o = argv[2][0];
+
 	if (argc == 4)
 	{
 		int1 = atoi(argv[1]);
 		int2 = atoi(argv[3]);
-		o = argv[2][0]
+		
 
-		if (o == '/' || o == '%)
-		{
-			if (int2 == 0)
-			{
-				printf("Error\n");
-				exit(100);
-			}
-		}
-		else if (o == '+' || o == '-' || o == '*')
-		{
-			result = (*get_op_func(o))(int1, int2);
-		}
-		else
-		{
-			printf("Error\n");
-			exit(99);
-		}
+		
+		result = (*get_op_func(argv[2]))(int1, int2);
 		printf("%d\n", result);
 	}
 	else
