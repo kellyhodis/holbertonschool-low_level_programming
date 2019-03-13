3849e7c181390692d646e15c052d507f99783136
Kelly Hodis <668@holbertonschool.com>
2019-03-13 21:19:28 +0000

    Added size_t macro

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNC_PTR_H
 #define FUNC_PTR_H
 
+#define size_t unsigned long
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
