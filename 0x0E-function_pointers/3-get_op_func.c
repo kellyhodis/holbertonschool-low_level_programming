4879ca58b98dae8558278ed543434e26dc0c4a7a
Kelly Hodis <668@holbertonschool.com>
2019-03-13 23:20:37 +0000

    First commit

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+* get_op_func - selects correct function to perform operation
+* @s: operator passed as argument
+*
+* Return: function pointer
+*/
+int (*get_op_func(char *s))(int, int)
+{
+
+
+
+
+
+
+}
