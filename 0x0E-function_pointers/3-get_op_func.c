32509948eee73717a9b77779133cd6db0b6599c5
kellyhodis <668@holbertonschool.com>
2019-03-13 19:46:58 -0400

    Update 3-get_op_func.c

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -8,7 +8,36 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	
+		
+	if (o == '+' || o == '-' || o == '*')
+	{
+		if (o == '+')
+			return (
+	}
+	else if (o == '/' || o == '%')
+	{
+		if (int2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		result = (*get_op_func(argv[2]))(int1, int2);
+	}
+	else
+	{
+	printf("Error\n");
+	exit(99);
+	}
 
 
 
