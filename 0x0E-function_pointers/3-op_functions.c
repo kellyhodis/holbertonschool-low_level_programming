f75e05deebfca0ef9c3004f40e3fc576470123bf
kellyhodis <668@holbertonschool.com>
2019-03-14 01:18:59 -0400

    Removed error checks

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -42,13 +42,7 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	else
-		return (a / b);
+	return (a / b);
 }
 /**
 * op_mod - returns remainder of division of a / b
@@ -59,11 +53,5 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	else
-		return (a % b);
+	return (a % b);
 }
