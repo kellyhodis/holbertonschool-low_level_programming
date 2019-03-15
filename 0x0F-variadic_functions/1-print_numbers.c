c63233b0c228dad907923de1f5fd2a38b91fedf1
Kelly Hodis <668@holbertonschool.com>
2019-03-15 14:04:52 +0000

    Added new line in case number of arguments is 0

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,7 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(listva, n);
 	for (i = 0; i < n; i++)
 	{
