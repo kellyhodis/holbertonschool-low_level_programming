5c0c2c498ea837acc6db69d614bf6a43be3c5443
Kelly Hodis <668@holbertonschool.com>
2019-03-15 19:28:10 +0000

    Combined two if statements into one and added helper functions

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -4,6 +4,62 @@
 #include <stdarg.h>
 #include <stdlib.h>
 /**
+* print_c - prints a char value
+* @c: char to print
+*
+* Return: nothing
+*/
+void print_c(char c)
+{
+        if (c)
+                printf("%c", c);
+}
+/**
+* print_numf - prints a float value
+* @num: float to print
+*
+* Return: nothing
+*/
+void print_numf(double num)
+{
+        printf("%f", num);
+}
+/**
+* print_string - prints a string
+* @s: string to print
+*
+* Return: nothing
+*/
+void print_string(char *s)
+{
+        if (s)
+                printf("%s", s);
+}
+/**
+* print_c - prints a char value
+* @c: char to print
+*
+* Return: nothing
+*/
+void print_numi(int c)
+{
+	printf("%d", c);
+}
+/**
+* get_type - calls the correct function based on type
+* @vartype: character that stands for variable type
+*
+* Return: nothing
+*/
+void get_type(char c, int param)
+{
+	if (c == 'c')
+		print_c(param);	
+	else if (c == 'i')
+		print_numi(param);
+}
+
+/**
 * print_all - prints anything
 * @format: list of types of arguments passed to the function
 *
@@ -16,29 +72,28 @@ void print_all(const char * const format, ...)
         void *memallo = malloc(strlen(format) + 1);
 	char *form = memallo;
 	char *iterate;
+	int param;
+
         strcpy(form, format);
         va_start(listva, format);
         while (form[i] != '\0')
 	{
-                if (form[i] == 'c')
+		if (form[i] == 'c' || form[i] == 'i')
 		{
-			printf("%c", va_arg(listva, int));
-
-		}
-		if (form[i] == 'i')
-		{
-			printf("%i", va_arg(listva, int));
+			param = va_arg(listva, int);
+			get_type(form[i], param);
 		}
 		if (form[i] == 'f')
 		{
-			printf("%f", va_arg(listva, double));
+			print_numf(va_arg(listva, double));
 		}
+
 		if (form[i] == 's')
 		{
 			iterate = va_arg(listva, char*);
 			if (!iterate)
 				iterate = "(nil)";
-			printf("%s", iterate);
+			print_string(iterate);
 		}
                 i++;
         }
