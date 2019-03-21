48d5d82cc7891f6c866fb20e96fedcb69e54d3cf
Kelly Hodis <668@holbertonschool.com>
2019-03-21 20:53:25 +0000

    First commit

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+* add_node_end - adds a new node at the end of a list_t list
+* @head: pointer to head of list_t list
+* @str: string to duplicate
+*
+* Return: address of new element or NULL if failure
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *last = *head;
+	int i;
+
+	new = (list_t *)malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	if (!(*head))
+	{
+		*head = new;
+		return (NULL);
+	}
+	while (last->next)
+		last = last->next;
+	last->next = new;
+	return (new);
+
+}
