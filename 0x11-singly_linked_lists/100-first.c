992f758a31699eb37e2641177adaf7e65f5acc98
Kelly Hodis <668@holbertonschool.com>
2019-03-21 21:31:31 +0000

    First commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* startup_fun - function that prints text before main is executed
+*/
+void startup_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+* cleanup_fun - function that cleans up previously called constructor
+*/
+void cleanup_fun(void)
+{
+}
