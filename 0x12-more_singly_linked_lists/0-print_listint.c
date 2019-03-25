b40e130fe9ad940d037a04a38d4bcceadd055cc1
Kelly Hodis <668@holbertonschool.com>
2019-03-25 20:53:56 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a listint_t list
+* @h: listint_t list to print elements of
+*
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	/* count nodes */
+	size_t count = 0;
+
+	/* checks if struct exists */
+	if (!h)
+		return (0);
+	/* as long as the current node points to another node */
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
