44670a1adaa0812fb439a6c9e8d94d93df9d4663
Kelly Hodis <668@holbertonschool.com>
2019-03-25 21:55:10 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a listint_t list
+* @head: pointer to the list to free
+*
+* Return: nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+	/* placeholder */
+	listint_t *place;
+
+	/* perform only if head exists */
+	while (*head)
+	{
+		/* set place to point to same location as head */
+		place = *head;
+		/* move head to point to next node's pointer */
+		*head = (*head)->next;
+		/* free current node */
+		free(place);
+	}
+	/* set head to point to NULL */
+	*head = NULL;
+}
