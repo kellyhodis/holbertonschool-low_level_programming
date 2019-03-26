f9d0ec37d3bbdca26d3d94ced4052a70f5bd6660
Kelly Hodis <668@holbertonschool.com>
2019-03-26 04:22:46 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strstr - locates a substring within a string
+* @haystack: string to search
+* @needle: substring to match
+*
+* Return: pointer to beginning of located substring or NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *p;
+	int flag = 0, i, j, k;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		k = i;
+		for (j = 0; needle[j]; j++)
+		{
+			while (haystack[k] == needle[j])
+			{
+				if (flag == 0)
+				{
+					p = &haystack[k];
+					flag = 1;
+				}
+				j++;
+				k++;
+				if (needle[j + 1] == '\0')
+					return (p);
+			}
+			flag = 0;
+			break;
+		}
+	}
+	return (NULL);
+}
