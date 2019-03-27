927c19da7cd4a4cffabbabc0c84674fa5013e97a
Kelly Hodis <668@holbertonschool.com>
2019-03-27 03:46:55 +0000

    Removed comments to conform to Betty function length requirements

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * insert_nodeint_at_index - inserts a new node at a given position
 * @head: pointer to the first node of the linked list
@@ -8,48 +7,34 @@
 *
 * Return: pointer to the new node
 */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/* new node to insert */
 	listint_t *new;
-	/* placeholder node */
 	listint_t *place = *head;
-	/* number to compare to index */
 	unsigned int count = 0;
 
 	if (!head)
 		return (NULL);
-	/* allocate memory to new node */
 	new = malloc(sizeof(listint_t));
-	/* if malloc fails, return NULL */
 	if (!new)
 		return (NULL);
-	/* assign data to new node */
 	new->n = n;
-	/* while head does not point to NULL and index hasn't been reached */
 	while (*head && count != idx)
 	{
-		/* if the next node is at the given index */
 		if (count == idx - 1)
 		{
-			/* point the new node to the current node's next */
 			new->next = (*head)->next;
-			/* point the current node's next to new instead */
 			(*head)->next = new;
 		}
-		/* traverse the linked list */
 		else
 			*head = (*head)->next;
 		count++;
 	}
-	/* if the number of nodes do not amount to desired index return NULL */
 	if (count != idx)
 	{
 		free(new);
 		return (NULL);
 	}
-	/* point head back to first node */
 	*head = place;
 	return (new);
 }
