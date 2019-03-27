a1683ba67a79001fc90705cf7a091b55b0248cdc
Kelly Hodis <668@holbertonschool.com>
2019-03-27 23:47:09 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_number - prints a number
+* @n: number to print
+*
+* Return: nothing
+*/
+void print_number(int n)
+{
+	int i, remain, num, x, j = 0, divide = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	remain = n % 10;
+	num = n;
+	for (i = 0; num / 10 != 0; i++)
+		num /= 10;
+	while (j < i)
+	{
+		divide *= 10;
+		j++;
+	}
+	while (divide > 1)
+	{
+		x = (n / divide) % 10;
+		_putchar(x + '0');
+		divide /= 10;
+	}
+	_putchar(remain + '0');
+}
