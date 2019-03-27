4cd5def0b3a6514d1fdec5fa9e81ee1f94817524
Kelly Hodis <668@holbertonschool.com>
2019-03-27 04:31:24 +0000

    Removed *head assignment when assigning new node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = (*head)->next;
 			(*head)->next = new;
-			*head = place;
+
 			return (new);
 		}
 		else
