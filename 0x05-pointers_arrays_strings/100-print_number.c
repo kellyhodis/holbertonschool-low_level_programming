50341de9263429d13b76811bfdc42bc31f632180
Kelly Hodis <668@holbertonschool.com>
2019-03-28 00:08:35 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <limits.h>
+/**
+* print_number - prints a number
+* @n: number to print
+*
+* Return: nothing
+*/
+void print_number(int n)
+{
+	int i, num, remain, x, j = 0, divide = 1, flag = 0;
+
+	if (n == INT_MIN)
+	{
+		flag = 1;
+		n = INT_MIN + 1;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	remain = n % 10;
+	num = n;
+	for (i = 0; num / 10 != 0; i++)
+		num /= 10;
+	while (j < i)
+	{
+		divide *= 10;
+		j++;
+	}
+	while (divide > 1)
+	{
+		x = (n / divide) % 10;
+		_putchar(x + '0');
+		divide /= 10;
+	}
+	if (flag == 1)
+		_putchar(remain + '1');
+	else
+		_putchar(remain + '0');
+}
