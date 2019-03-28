641cd54fd7f4666007e57cc8b65b4c61081e94e2
Kelly Hodis <668@holbertonschool.com>
2019-03-28 21:33:24 +0000

    First commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* get_bit - returns the value of a bit at a given index
+* @n: number
+* @index: index of the desired bit
+*
+* Return: value of the bit or -1 if error
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8 - 1)
+		return (-1); 
+	return ((n >> index) & 1);	
+}
