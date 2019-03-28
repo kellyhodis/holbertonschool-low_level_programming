b8f7e3935b80f594019834f1258583868e4c842e
Kelly Hodis <668@holbertonschool.com>
2019-03-28 21:34:24 +0000

    Edited styling

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,6 +9,6 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > sizeof(n) * 8 - 1)
-		return (-1); 
-	return ((n >> index) & 1);	
+		return (-1);
+	return ((n >> index) & 1);
 }
