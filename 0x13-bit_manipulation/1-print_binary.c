7fe1695846971c5c2da8effcee8891e54bdf4f03
Kelly Hodis <668@holbertonschool.com>
2019-03-28 20:54:13 +0000

    First commit

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* print_binary - prints the binary representation of a number
+* @n: number to represent
+*
+* Return: nothing
+*/
+void print_binary(unsigned long int n)
+{
+	int c = 0;
+	unsigned long int shift, decimal = 1;
+
+	while (decimal <= n)
+	{
+		decimal *= 2;
+		c++;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	for (c = c - 1; c >= 0; c--)
+	{
+		shift = n >> c;
+
+		if (shift & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
