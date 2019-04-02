d67db7d50de32aa58257ee62d1c52f75a71cc660
Kelly Hodis <668@holbertonschool.com>
2019-04-02 20:15:11 +0000

    Freed malloc and closed fd

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -16,8 +16,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, 0);
 	read_ = read(fd, buf, letters);
+	close(fd);
 	write_ = write(1, buf, read_);
 	if (read_ != write_)
+	{
+		free(buf);
 		return (0);
+	}
+	free(buf);
 	return (read_);
 }
