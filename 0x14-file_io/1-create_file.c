01fdf3baa601287def606a6f8b9076a2f843d6db
Kelly Hodis <668@holbertonschool.com>
2019-04-02 22:36:24 +0000

    First commit

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* create_file - creates a file
+* @filename: name of file to create
+* @text_content: string to write to file
+*
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t write_;
+	int fd, i;
+
+	for (i = 0; text_content[i]; i++)
+		;
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write_ = write(fd, text_content, i);
+	close(fd);
+	if (write_ == -1)
+		return (-1);
+	return (1);
+}
