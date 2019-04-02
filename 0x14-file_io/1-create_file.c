afb055569f42f0db9b7edf362635f8ac47cd7252
Kelly Hodis <668@holbertonschool.com>
2019-04-02 23:21:34 +0000

    Changed location of string parse

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,8 +11,6 @@ int create_file(const char *filename, char *text_content)
 	ssize_t write_ = 0;
 	int fd, i;
 
-	for (i = 0; text_content[i]; i++)
-		;
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -20,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
+		for (i = 0; text_content[i]; i++)
+			;
 		write_ = write(fd, text_content, i);
 		if (write_ == -1)
 			return (-1);
