7b5e1df78bdbde5e5f67fc263e9584771916b2d1
Kelly Hodis <668@holbertonschool.com>
2019-04-02 20:42:12 +0000

    Added libraries

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
