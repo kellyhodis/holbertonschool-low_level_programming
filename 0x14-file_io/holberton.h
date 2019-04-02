4dfdaf107482982b090a6ad3cff47923c8e888b4
Kelly Hodis <668@holbertonschool.com>
2019-04-02 20:51:27 +0000

    Replaced included sys library

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/uio.h>
+#include <sys/types.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
