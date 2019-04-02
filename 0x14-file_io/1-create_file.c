160ce4af91a46bcef3e17b7aaa82de1f38bd5f60
Kelly Hodis <668@holbertonschool.com>
2019-04-02 22:53:23 +0000

    Changed write condition back

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -8,14 +8,14 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t write_;
+	ssize_t write_ = 0;
 	int fd, i;
 
 	for (i = 0; text_content[i]; i++)
 		;
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
