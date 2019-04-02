ae87d944b43d830b0ec31189d30950dbfa1d1911
Kelly Hodis <668@holbertonschool.com>
2019-04-02 20:07:10 +0000

    Added libraries

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
