def3d6ebc754a5695060d602356bdc26e33d074f
Kelly Hodis <668@holbertonschool.com>
2019-04-02 22:45:48 +0000

    Added condition for when text_content is NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,8 +18,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-		write_ = write(fd, text_content, i);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	write_ = write(fd, text_content, i);
 	close(fd);
 	if (write_ == -1)
 		return (-1);
