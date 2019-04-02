b991a30bffc7b3d6fb80f65e55a5bd2a11f838af
Kelly Hodis <668@holbertonschool.com>
2019-04-02 23:15:51 +0000

    Changed condition for writing to file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,7 +10,6 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t write_ = 0;
 	int fd, i;
-
 	for (i = 0; text_content[i]; i++)
 		;
 	if (!filename)
@@ -18,13 +17,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
-		return (1);
+		write_ = write(fd, text_content, i);
+		if (write_ == -1)
+			return (-1);
 	}
-	write_ = write(fd, text_content, i);
 	close(fd);
-	if (write_ == -1)
-		return (-1);
 	return (1);
 }
