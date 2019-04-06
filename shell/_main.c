9f31b049fb30e4d267c42c610be30bd4615e9691
Kelly Hodis <668@holbertonschool.com>
2019-04-06 20:06:06 +0000

    Added tests for _unsetenv

--- shell/_main.c
+++ shell/_main.c
@@ -2,9 +2,20 @@
 
 int main(void)
 {	
-	printf("%d\n", setenv("TEST", "meaningless", 1));
-	printf("%d\n", _setenv("TEST2", "meaningFUL", 1));
+	printf("%d\n", setenv("TEST", "meaningless", 0));
+	printf("%d\n", _setenv("TEST2", "meaningFUL", 0));
 	printf("%s\n", getenv("TEST"));
 	printf("%s\n", _getenv("TEST2"));
+	printf("%d\n", unsetenv("TEST"));
+	printf("%d\n", _unsetenv("TEST2"));
+	if (getenv("TEST") == NULL)
+		printf("deleted\n");
+	else
+		printf("%s\n", getenv("TEST"));
+	if (_getenv("TEST2") == NULL)
+		printf("deleted\n");
+	else
+		printf("%s\n", _getenv("TEST2"));
+
 	return (0);
 }
