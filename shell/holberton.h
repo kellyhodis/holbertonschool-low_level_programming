3fa5a3264594db77c780fa0501e3661879342add
Kelly <668@holbertonschool.com>
2019-04-06 17:21:50 +0000

    First commit

--- shell/holberton.h
+++ shell/holberton.h
@@ -1,6 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    struct list_s *next;
+} list_t;
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,5 +23,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+
+list_t *add_node_end(list_t **head, const char *str);
+char *_getenv(const char *name);
 
 #endif /* HOLBERTON_H */
--- shell/print_path.c
+++ shell/print_path.c
@@ -4,12 +4,15 @@ int main(void)
 {
 	char *token;
 	char *parse = NULL;
+	list_t *head;
 
+	head = NULL;
 	parse = _getenv("PATH");
 	token = strtok(parse, ":");
 	while (token != NULL)
 	{
 		printf("%s\n", token);
+		add_node_end(&head, token);
 		token = strtok(NULL, ":");
 	}
 
