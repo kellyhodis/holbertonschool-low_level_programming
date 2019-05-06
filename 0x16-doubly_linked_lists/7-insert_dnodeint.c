ac8b883a7bdbf77fdb0da1d2e08634b5bf259f7b
Kelly Hodis <668@holbertonschool.com>
2019-05-06 01:32:42 +0000

    Added cases for when inserting at idx 0 or at the end of a list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,9 +19,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-
 	count = 0;
+	new->n = n;
 	traverse = *h;
+	if (traverse && count == idx)
+	{
+		new->prev = NULL;
+		new->next = traverse;
+		traverse->prev = new;
+		*h = new;
+		return (new);
+	}
 	while (traverse && count != idx)
 	{
 		count++;
@@ -30,12 +38,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->next = traverse->next;
 			new->prev = traverse;
 			traverse->next = new;
-			new->n = n;
+			return (new);
+		}
+		if (!traverse->next && count + 1 == idx)
+		{
+			traverse->next = new;
+			new->prev = traverse;
+			new->next = NULL;
 			return (new);
 		}
 		traverse = traverse->next;
 	}
-
 	free(new);
 	return (NULL);
 }
