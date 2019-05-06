d398bb29468b63d1a063d648645f53e635f657ae
Kelly Hodis <668@holbertonschool.com>
2019-05-06 02:17:26 +0000

    Added statement to set previous node to new node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (!traverse->next)
 				new->next = NULL;
 			else
+			{
 				new->next = traverse->next;
+				traverse->prev = new;
+			}
 			new->prev = traverse;
 			traverse->next = new;
 			return (new);
