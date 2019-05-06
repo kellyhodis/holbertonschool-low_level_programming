91bf9fba957b98ca0c0f482342c34eeb53527acc
Kelly Hodis <668@holbertonschool.com>
2019-05-06 01:35:54 +0000

    Removed check for null h

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *traverse;
 	unsigned int count;
 
-	if (!*h)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
