5c460c72848caef2708dc1f72331856bb4397d8d
Kelly Hodis <668@holbertonschool.com>
2019-05-06 00:44:17 +0000

    Added helper function inside file

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+* add_dnodeint - add a new node at the beginning of a doubly linked list
+* @head: head of the list
+* @n: number to insert in the new node
+*
+* Return: address of new element or NULL
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
+
+/**
 * add_dnodeint_end - adds a new node at the end of a doubly linked list
 * @head: head of list
 * @n: number to place in element
