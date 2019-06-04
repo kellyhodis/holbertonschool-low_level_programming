170819bf54236f6f099ced1e2a56a0b034f86942
Kelly Hodis <668@holbertonschool.com>
2019-06-04 22:49:59 +0000

    Fixed function and styling

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,33 +1,38 @@
-#include "hash_table.h"
-/* be sure to keep memory clean! */
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table struct
+ * @size: size of the table
+ *
+ * Return: pointer to hash table
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	/* declare variables */
 	hash_table_t *new_table;
-	hash_node_t *new_node;
+	hash_node_t **new_array;
 
-	/* allocate memory for the hash table */ 
-	new = malloc(sizeof(hash_table_t));
+	/* allocate memory for the hash table */
+	new_table = malloc(sizeof(hash_table_t));
 
 	/* check for memory allocation error */
 	if (!new_table)
 		return (NULL);
 
-	/* allocate memory for each element of the array
-	   the array should have size elements */
-	/* assign values / initialize data inside each node */
-	for (i = 0; i < size; i++)
-	{ 
-		new_node = malloc(sizeof(hash_node_t));
-		if (!new_node)
-		{
-			free(new_table);
-			return (NULL);
-		}
-		
-		/* error check */
+	/* allocate memory for the hash node */
+	new_array = malloc(size);
+
+	/* check for memory allocation error */
+	if (!new_array)
+	{
+		free(new_table);
+		return (NULL);
 	}
+
+	/* assign values to table struct */
+	new_table->size = size;
+	new_table->array = new_array;
 	/* return a pointer to a hash table */
 	return (new_table);
 }
