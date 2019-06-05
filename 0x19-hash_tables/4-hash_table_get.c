a299bf6ed473be9bbf631d3a7ae09c53f84564f9
Kelly Hodis <668@holbertonschool.com>
2019-06-05 06:19:21 +0000

    Added function

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,10 +1,26 @@
-#include "hash_table.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	/* declare variables */
+	unsigned long int index = 0;
 
-	/* find key */
+	index = hash_djb2((const unsigned char *)key);
 
-	/* return a string that contains the value with associated key */
+	while (ht->array[index]->next)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	if (strcmp(key, (ht->array[index])->key) == 0)
+		return (ht->array[index]->value);
+	return (NULL);
 }
