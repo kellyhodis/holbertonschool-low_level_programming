04b92e24d774659fd821c6afb06e7855c388008a
Kelly Hodis <668@holbertonschool.com>
2019-06-05 01:15:50 +0000

    Added function

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,11 +1,26 @@
-#include "hash_table.h"
-/* be sure to keep memory clean! */
+#include "hash_tables.h"
+
+/**
+ * key_index - returns index of a key
+ * @key: key
+ * @size: size of the array of the hash table
+ *
+ * Return: index at which key/value pair stored in array
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	/* declare variables */
+	unsigned long int index = 0;
 
-	/* search for index containing key */
+	/* compute index by using hash function */
+	if (key)
+		index = hash_djb2(key);
+
+	/* adjust index for size of array */
+	if (size != 0)
+		index = index % size;
 
 	/* return an unsigned long int representing index of key */
+	return (index);
 }
