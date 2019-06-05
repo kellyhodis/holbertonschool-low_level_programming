c1905f58021e7a8f96ec798a9eada6bc749492ef
Kelly Hodis <668@holbertonschool.com>
2019-06-05 05:51:33 +0000

    Added function

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,14 +1,49 @@
-#include "hash_table.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key of element
+ * @value: value of element
+ *
+ * Return: 1 or 0
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* declare variables */
+	hash_node_t *new;
+	unsigned long int index = 0;
+
+	/* key cannot be empty string */
+	if (!key || key[0] == '\0') 
+		return (0);
 
 	/* allocate memory for new element */
+	new = malloc(sizeof(hash_node_t));
 
 	/* check for allocation errors */
+	if (!new)
+		return (0);
 
-	/* assign new element to correct index */
+	/* make copies of key and value */
+	new->key = strdup(key);
+	new->value = strdup(value);
 
-	/* return an integer that returns 0 or 1 */
+	/* get index of key */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/*  add node to array if no node at index exists */
+	if (ht->array[index] == NULL)
+		ht->array[index] = new;
+
+	/* push node to front of list if node exists */
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+
+	/* return success */
+	return (1);
 }
