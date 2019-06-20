1049671416c9638a24424f897a6a09cc0f29144d
Kelly Hodis <668@holbertonschool.com>
2019-06-20 04:03:24 +0000

    First commit

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers using Quick sort algorithm
+ * Lomuto partition scheme
+ * @array: array of integers to sort
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
