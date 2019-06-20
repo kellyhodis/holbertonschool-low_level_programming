9c16cc2d9dae675bddc31572ca623ba204d57e3c
Kelly Hodis <668@holbertonschool.com>
2019-06-20 00:37:57 +0000

    First commit

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts array of integers using selection sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int n, i, idx;
+	int smallest_num;
+	int flag = 1;
+
+	n = 0;
+	while (flag)
+	{
+		for (i = n; i < size; i++)
+		{
+			if (i == n)
+			{
+				smallest_num = array[i];
+			}
+			if (array[i] < smallest_num)
+			{
+				smallest_num = array[i];
+				idx = i;
+			}
+			if (n == size - 1)
+				flag = 0;
+		}
+		if (flag == 0)
+			break;
+		n++;
+		if (idx != n - 1)
+		{
+			array[idx] = array[n - 1];
+			array[n - 1] = smallest_num;
+			print_array(array, size);
+		}
+	}
+}
