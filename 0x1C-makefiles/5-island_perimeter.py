e820e13b4f749aa352b410a6ab22d4a81fe51168
Kelly Hodis <668@holbertonschool.com>
2019-07-12 07:14:31 +0000

    Update function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,19 +7,24 @@ def island_perimeter(grid):
     ''' Function to calculate the perimeter of an island
     given a grid that represents it. '''
     perimeter = 0
-    for j in range(len(grid)):
-        for i in range(len(grid[0])):
-            connect_check = perimeter
-            if grid[j][i] == 1:
-                perimeter += 1
-                if i == 0 or i == len(grid[0]) - 1:
+    for row in range(len(grid)):
+        connect_check = perimeter
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if col == 0 or col == len(grid[0]) - 1:
                     perimeter += 1
-                elif grid[j - 1][i] == 0 or grid[j + 1][i] == 0:
+                else:
+                    if grid[row][col - 1] == 0:
+                        perimeter += 1
+                    if grid[row][col + 1] == 0:
+                        perimeter += 1
+                if row == 0 or row == len(grid) - 1:
                     perimeter += 1
-                if j == 0 or j == len(grid) - 1:
-                    perimeter += 1
-                elif grid[j][i - 1] == 0 or grid[j][i + 1] == 0:
-                    perimeter += 1
-            if perimeter != 0 and connect_check == perimeter:
-                return perimeter
+                else:
+                    if grid[row - 1][col] == 0:
+                        perimeter += 1
+                    if grid[row + 1][col] == 0:
+                        perimeter += 1
+        if perimeter != 0 and connect_check == perimeter:
+            return perimeter
     return perimeter
