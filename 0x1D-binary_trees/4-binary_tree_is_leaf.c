a76b40268a15cdbab98596094b52c911da34758e
Kelly <668@holbertonschool.com>
2019-07-23 23:05:11 +0000

    Add function

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -9,7 +9,11 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	(void)node;
+	if (!node)
+		return (0);
 
-	return (0);
+	if (!node->left && !node->right)
+		return (1);
+	else
+		return (0);
 }
