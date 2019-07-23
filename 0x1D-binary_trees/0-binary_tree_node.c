9d1ce1c9059255d8229c5dee2eeea3388469ed41
Kelly <668@holbertonschool.com>
2019-07-23 22:22:49 +0000

    Add function

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,4 +1,4 @@
-#include binary_trees.h
+#include "binary_trees.h"
 
 /**
  * binary_tree_node - create a binary tree node
@@ -10,8 +10,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	(void)parent;
-	(void)value;
+	binary_tree_t *new_node;
 
-	return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;	
+
+	return (new_node);
 }
