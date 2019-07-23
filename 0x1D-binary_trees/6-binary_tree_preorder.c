2e680a4666ef24134fe21ece1ea2680fd514ff55
Kelly <668@holbertonschool.com>
2019-07-23 23:45:54 +0000

    Add function

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -10,6 +10,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	(void)tree;
-	(void)func;
+	if (tree)
+	{
+		func(tree->n);
+		if (tree->left)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right)
+			binary_tree_preorder(tree->right, func);
+	}
 }
