dea69fd4ecf1c4135a57ef546cb5d20d18f380c3
Kelly Hodis <668@holbertonschool.com>
2019-07-23 07:19:25 +0000

    Create file

---
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measure the depth of a node in a binary tree
+ * @tree: pointer to the node to measure depth
+ *
+ * Return: 0 or 1
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	(void)tree;
+
+	return (0);
+}
