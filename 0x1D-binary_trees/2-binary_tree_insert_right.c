80f92246b4a03c812b0fc9d8758a3e8323232c01
Kelly Hodis <668@holbertonschool.com>
2019-07-23 06:44:10 +0000

    Create file

---
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts node as right-child of another node
+ * @parent: pointer to node to insert right-child in
+ * @value: value to store in new node
+ *
+ * Return: pointer to created node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	(void)parent;
+	(void)value;
+
+	return (NULL);
+}
