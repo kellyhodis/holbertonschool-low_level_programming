062fb3cb049b8145355f6a516501074989cd8e08
Kelly <668@holbertonschool.com>
2019-07-23 23:03:14 +0000

    Add function

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -9,5 +9,12 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	(void)tree;
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_delete(tree->left);
+		if (tree->right)
+			binary_tree_delete(tree->right);
+		free(tree);	
+	}
 }
