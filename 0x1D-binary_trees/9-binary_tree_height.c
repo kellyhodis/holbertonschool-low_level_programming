d98ea6a69e85f50b11679d34afb21dbd512658f8
Kelly Hodis <668@holbertonschool.com>
2019-07-23 07:15:37 +0000

    Create file

---
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 0 or 1
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	(void)tree;
+
+	return (0);
+}
