1c3d6205f261e962e99c3f01eb8d21aeb7dca376
Kelly Hodis <668@holbertonschool.com>
2019-07-23 17:37:26 +0000

    Create file

---
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to node
+ *
+ * Return: pointer to sibling node or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	(void)node;
+
+	return (NULL);
+}
