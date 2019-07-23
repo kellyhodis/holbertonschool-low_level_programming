2a48899952edaa9e9afc286e76bbed5336eb9177
Kelly Hodis <668@holbertonschool.com>
2019-07-23 06:47:07 +0000

    Create file

---
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node as left-child of another node
+ * @parent: pointer to node to insert left-child in
+ * @value: value to store in new node
+ *
+ * Return: pointer to created node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	(void)parent;
+	(void)value;
+
+	return (NULL);
+}
