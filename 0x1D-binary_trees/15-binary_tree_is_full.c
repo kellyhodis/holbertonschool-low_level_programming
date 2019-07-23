d07cd0b6975b3debc68edbf90a32bc52a246a007
Kelly Hodis <668@holbertonschool.com>
2019-07-23 17:33:18 +0000

    Create file

---
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to root node of tree
+ *
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	(void)tree;
+
+	return (0);
+}
