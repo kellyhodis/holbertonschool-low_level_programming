e1bd2dd1fb47d95afeece15105b5121912d60967
Kelly <668@holbertonschool.com>
2019-07-23 22:53:33 +0000

    Add functiong

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -10,8 +10,27 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	(void)parent;
-	(void)value;
+	binary_tree_t *new_right_child;
 
-	return (NULL);
+	if (!parent)
+		return (NULL);
+
+	new_right_child = malloc(sizeof(binary_tree_t));
+	if (!new_right_child)
+		return (NULL);
+
+	if (parent->right)
+	{
+		new_right_child->right = parent->right;
+		new_right_child->right->parent = new_right_child;
+	}
+	else
+		new_right_child->right = NULL;
+
+	parent->right = new_right_child;
+	new_right_child->parent = parent;
+	new_right_child->left = NULL;
+	new_right_child->n = value;
+
+	return (new_right_child);
 }
