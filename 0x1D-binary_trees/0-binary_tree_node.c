c780e970956836a1a0de11b649c6191c5a56ebd6
Kelly Hodis <668@holbertonschool.com>
2019-07-23 06:14:18 +0000

    Update file

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include binary_trees.h
+
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: parent node of node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to new node or NULL
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	(void)parent;
+	(void)value;
+
+	return (NULL);
+}
