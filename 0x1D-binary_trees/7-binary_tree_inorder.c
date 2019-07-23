80447c39316cb7af2988131060e95e6a93a6cf0a
Kelly <668@holbertonschool.com>
2019-07-23 23:50:16 +0000

    Add function

--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -10,6 +10,12 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	(void)tree;
-	(void)func;
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		if (tree->right)
+			binary_tree_inorder(tree->right, func);
+	}
 }
