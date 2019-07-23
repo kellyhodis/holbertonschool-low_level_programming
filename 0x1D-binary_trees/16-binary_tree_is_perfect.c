38ca9ae1aca1556a254ca6afd82d198b2ff0cb9e
Kelly Hodis <668@holbertonschool.com>
2019-07-23 17:35:12 +0000

    Create file

---
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to root node of tree
+ *
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	(void)tree;
+
+	return (0);
+}
