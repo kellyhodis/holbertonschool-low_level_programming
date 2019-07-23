390c386f3ede8d5ee8403aa20054906574a70f60
Kelly Hodis <668@holbertonschool.com>
2019-07-23 07:08:51 +0000

    Create file

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to function to call for each node
+ *
+ * Return: nothing
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	(void)tree;
+	(void)func;
+}
---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes through binary tree using post-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to function to call for each node
+ *
+ * Return: nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	(void)tree;
+	(void)func;
+}
