dd33a65a39a1b609e24c43405354cdb1a847b939
Kelly <668@holbertonschool.com>
2019-07-24 00:45:20 +0000

    Add function

--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -4,12 +4,36 @@
  * binary_tree_size - function that measures the size of a binary tree
  * @tree: pointer to the root node of the tree to measure the size
  *
- * Return: 0 or 1
+ * Return: 0 or size
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	(void)tree;
+	int size;
+	const binary_tree_t *traverse;
 
-	return (0);
+	/* add conditions for following nodes to right in left while loop
+	   add conditions for following nodes to left in right while loop */
+	if (tree)
+	{
+		size = 1;
+		traverse = tree;
+		while (traverse->left)
+		{
+			size++;
+			if (traverse->left->right)
+				size++;
+			traverse = traverse->left;
+		}
+		traverse = tree;
+		while (traverse->right)
+		{
+			size++;
+			if (traverse->right->left)
+				size++;
+			traverse = traverse->right;
+		}
+	}
+
+	return (size);
 }
