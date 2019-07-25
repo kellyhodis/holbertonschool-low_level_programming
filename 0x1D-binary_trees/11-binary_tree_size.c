9b9c33901a1ed5bc0a7ad4313ab58aa9302990ca
Kelly <668@holbertonschool.com>
2019-07-25 02:27:48 +0000

    Edit styling

--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -15,6 +15,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!(tree->left) && !(tree->right))
 		return (1);
 
-	return (1 + binary_tree_size(tree->left) \
-		+ binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
